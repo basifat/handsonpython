19fb9fdb6c41b2e81dfff37d9bf599ccabb9f770
basifat <code@babatundeasifat.com>
2020-08-14 20:07:35 +0100

    Updating commit

--- tuples_structures.py
+++ tuples_structures.py
@@ -1,26 +1,26 @@
-# tp = 12345, 54321, 'hello!'
-# print(tp)
+tp = 12345, 54321, 'hello!'
+print(tp)
 
 
-# def test(a):
-#     return [a,10,15,16]
+def test(a):
+    return [a,10,15,16]
 
-# x,y,z,b =test(5)
-# print(x)
-# print(y)
-# print(z)
+x,y,z,b =test(5)
+print(x)
+print(y)
+print(z)
 
-# student = ("Tunde", 70)
-# student2 = ("Abel", 70)
+student = ("Tunde", 70)
+student2 = ("Abel", 70)
 
-# # Dictionaries
-# dico = dict([
-#     student,
-#     student2
-# ])
+# Dictionaries
+dico = dict([
+    student,
+    student2
+])
 
 
-#print(dico)
+print(dico)
 
 banana = ("banana", 4)
 apple = ("apple", 2)
