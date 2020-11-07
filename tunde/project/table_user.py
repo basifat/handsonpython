01094de0274273264c0ab5dcc8d853db90dfd96e
Teeomole <eleganttd@gmail.com>
2020-11-07 12:22:32 +0000

    Removed unsued tunde foler

--- tunde/project/table_user.py
+++
@@ -1,8 +0,0 @@
-from tunde.project.students.student_table import StudentTable
-
-
-example_stable =  StudentTable()
-result = example_stable.create(entity_id=1011, gpa=4.2, full_name="James")
-print(result)
-
-
--- tunde/yaas_project/yaas_ecommerce/yaasusers/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class YaasusersConfig(AppConfig):
-    name = 'yaasusers'
